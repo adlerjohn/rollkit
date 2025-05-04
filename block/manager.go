246f3ba2d3638296054d21f7e59e2e43fb25b599
Manav Aggarwal <manavaggarwal1234@gmail.com>
2025-05-04 22:40:54 +0100

    Add height > 1 condition

--- block/manager.go
+++ block/manager.go
@@ -729,7 +729,7 @@ func (m *Manager) execValidate(lastState types.State, header *types.SignedHeader
 
 	// Verify that the header's timestamp is strictly greater than the last block's time
 	headerTime := header.Time()
-	if lastState.LastBlockTime.After(headerTime) {
+	if header.Height() > 1 && lastState.LastBlockTime.After(headerTime) {
 		return fmt.Errorf("block time must be strictly increasing: got %v, last block time was %v",
 			headerTime.UnixNano(), lastState.LastBlockTime)
 	}
